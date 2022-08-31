789f56cd1a739f60aed669cf86325669d154e342
vonfintel <49501069+vonfintel@users.noreply.github.com>
2022-08-31 00:58:08 -0400

    Add project files.

---
+++ Gamestate.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind;
+
+public class Gamestate
+{
+    public List<int> answerNumbers = new List<int>();
+    public bool isWon = false;
+    //public List<char> winCondition = new List<char>();
+    //public List<char> correctNumbers = new List<char>();
+    Random rnd = new Random();
+        
+    public Gamestate()
+    {
+        CreateAnswer();
+    }
+    //generates the answer key for the player to guess (no duplicates)
+    public void CreateAnswer() 
+    { 
+        for(int i = 0; answerNumbers.Count < 4; i++){
+            int randomNumber = rnd.Next(1,7);
+            if (!answerNumbers.Contains(randomNumber)){
+                answerNumbers.Add(randomNumber);
+            }
+        }    
+
+    }
+    //checks for userinput and makes sure it is in the correct format
+    public bool CheckForValidAnswer(string userAnswer)
+    {
+        if (userAnswer.Length != 4)
+        {
+            return false;
+        }
+        for (int i = 0; i < userAnswer.Length; i++)
+        {
+            if (!Char.IsDigit(userAnswer[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void AnswerCheck(string userAnswer) {
+        List<char> winCondition = new List<char>();
+        List<char> correctNumbers = new List<char>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                if (answerNumbers[i] == Char.GetNumericValue(userAnswer[i]))
+                {
+                    winCondition.Add('+');
+                    
+                    break;
+                }
+                else if (answerNumbers[i] == Char.GetNumericValue(userAnswer[j]))
+                {
+                    correctNumbers.Add('-');
+                    
+                }
+
+            
+            
+            }
+        
+        }
+        Console.Write(string.Join("", winCondition));
+        Console.Write(string.Join("", correctNumbers));
+        Console.WriteLine();
+        if (winCondition.Count == 4) 
+        {
+            isWon = true;
+        }
+
+    }
+
+}   
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Mastermind;
+
+class Caller
+{
+    public static void Main(string[] args)
+    {   
+        Gamestate gamestate = new Gamestate();
+        int tries = 10;
+        string userInput = "";
+        //
+        Console.Write("Answer Key: ");
+        foreach (int number in gamestate.answerNumbers) {
+            Console.Write(number);
+        
+        }
+        //
+        Console.WriteLine();
+        while (tries != 0) {
+            while (!gamestate.CheckForValidAnswer(userInput))
+            {
+                Console.WriteLine("Please enter your four digit guess");
+                userInput = Console.ReadLine();
+            }
+            
+            gamestate.AnswerCheck(userInput);
+
+            if (gamestate.isWon) 
+            {
+                Console.WriteLine("Correct! The player wins!");
+                break;
+            }
+            userInput = "";
+            tries --;
+            if (tries == 0)
+            {
+                Console.WriteLine("Thats 10 tries! The player lost. The answer was " + string.Join("", gamestate.answerNumbers));
+            }
+        }
+
+      
+        
+
+
+    }
+}
